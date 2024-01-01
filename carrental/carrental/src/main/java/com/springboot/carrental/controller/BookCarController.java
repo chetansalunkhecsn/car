ee18cc8b7fe43dfe2de31a470c0ce482b3d3e101
chetansalunkhecsn <chetansalunkhecsn@gmail.com>
2024-01-01 19:22:57 +0530

    firstgit

---
+++ carrental/carrental/src/main/java/com/springboot/carrental/controller/BookCarController.java
@@ -0,0 +1,19 @@
+package com.springboot.carrental.controller;
+
+import com.springboot.carrental.dto.BookCarPayloadDto;
+import com.springboot.carrental.dto.BookCarResponseDto;
+import com.springboot.carrental.services.BookCarService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class BookCarController {
+
+    @Autowired
+    BookCarService bookCarService;
+
+    @PostMapping("/book-now/{car_id}")
+    public BookCarResponseDto bookNow(@RequestBody BookCarPayloadDto bookCarPayloadDto, @PathVariable Long car_id){
+        return bookCarService.bookCar(car_id,bookCarPayloadDto);
+    }
+}
